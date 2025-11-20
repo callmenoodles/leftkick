d354c38e515061391fb12fda30e9b06656e04b12
Noodles <noodles@udon.md>
2025-11-20 20:56:26 +0100

    refactor: move code to functions

--- scripts/kick.js
+++ scripts/kick.js
@@ -1,24 +1,22 @@
 function moveChat() {
-	const chat = document.getElementById("channel-chatroom");
+	const chatroom = document.getElementById("channel-chatroom");
 	const main = document.querySelector("main");
 	const messages = document.getElementById("chatroom-messages");
 	const scrollPosition = messages.scrollTop;
 
 	// Chat is inside main on mobile layouts
-	if (chat && main && !main.contains(chat)) {
+	if (chatroom && main && !main.contains(chatroom)) {
 		const btnExpand = main.querySelector("button");
-		const btnCollapse = chat.firstChild.querySelector("div");
 		const btnExpandText = btnExpand.querySelector("span");
 		const btnExpandIcon = btnExpand.querySelector("svg");
+		const btnCollapse = chatroom.firstChild.querySelector("div");
 
 		const storage =
 			typeof browser === "undefined" ? chrome.storage : browser.storage;
 
-		storage.local.get("direction").then((res) => {
-			const direction = res.direction;
-
-			if (direction === "left") {
-				main.before(chat);
+		storage.local.get("hasMoved").then((res) => {
+			if (res.hasMoved) {
+				main.before(chatroom);
 
 				if (btnExpand) {
 					btnExpandIcon.style.transform = "scale(-1,1)";
@@ -32,10 +30,10 @@ function moveChat() {
 				}
 
 				storage.local.set({
-					direction: "right",
+					hasMoved: false,
 				});
 			} else {
-				main.after(chat);
+				main.after(chatroom);
 
 				if (btnExpand) {
 					btnExpandIcon.style.transform = "scale(1, 1)";
@@ -49,7 +47,7 @@ function moveChat() {
 				}
 
 				storage.local.set({
-					direction: "left",
+					hasMoved: true,
 				});
 			}
 		});
@@ -58,126 +56,144 @@ function moveChat() {
 	}
 }
 
-function init() {
-	let currentUsername = null;
-	let currentChatroomParent = null;
-	let currentPlayerChildren = null;
+function moveUserCard() {
+	const userCard = document.getElementById("user-identity");
 
-	const observer = new MutationObserver(() => {
-		const usernameElement = document.getElementById("channel-username");
+	if (userCard) {
+		const chatWidth =
+			getComputedStyle(userCard).getPropertyValue("--chat-width");
+		const sidebar = document.getElementById("sidebar-wrapper");
+		let sidebarWidth = 0;
+
+		if (sidebar) {
+			sidebarWidth = sidebar.offsetWidth;
+		}
+
 		const storage =
 			typeof browser === "undefined" ? chrome.storage : browser.storage;
 
-		if (usernameElement) {
-			const username = usernameElement.innerText.trim();
+		storage.local.get("hasMoved").then((res) => {
+			if (!res.hasMoved) {
+				userCard.style.left = parseInt(chatWidth) + sidebarWidth + "px";
+			} else {
+				userCard.style.removeProperty("left");
+			}
+		});
+	}
+}
 
-			if (username !== currentUsername) {
-				currentUsername = username;
-				moveChat();
+let currentUsername;
+
+function moveOnStreamChange() {
+	const usernameElement = document.getElementById("channel-username");
+
+	if (usernameElement) {
+		const username = usernameElement.innerText.trim();
+
+		if (username !== currentUsername) {
+			currentUsername = username;
+			moveChat();
+		}
+	}
+}
+
+let currentPlayerChildren;
+
+// HOTFIX: More than 2 elements?
+// Theater mode
+function moveVideoPlayer() {
+	const video = document.getElementById("video-player");
+	const player = video.parentElement;
+	if (player) {
+		if (player.childElementCount !== currentPlayerChildren) {
+			currentPlayerChildren = player.childElementCount;
+			const storage =
+				typeof browser === "undefined" ? chrome.storage : browser.storage;
+
+			if (currentPlayerChildren > 1) {
+				storage.local.get("hasMoved").then((result) => {
+					const hasMoved = result.hasMoved ?? false;
+					const controls = player.lastChild;
+
+					if (!hasMoved) {
+						controls.classList.replace("left-0", "right-0");
+					} else {
+						controls.classList.replace("right-0", "left-0");
+					}
+				});
 			}
 		}
+	}
+}
 
-		const chat = document.getElementById("channel-chatroom");
+let currentChatroomParent;
 
-		if (chat) {
-			if (chat.parentElement !== currentChatroomParent) {
-				// FIXME: Button disappears when going into a submenu
-				// FIXME: Logout removes it
-				// FIXME: Zooming removes buttons
-				// FIXME: Multiple buttons rendering
-				// FIXME: Chat replay
-				// FIXME: Multiple tabs breaks sometimes (check side first)
+// FIXME: Button disappears when going into a submenu
+// FIXME: Logout removes it
+// FIXME: Zooming removes buttons
+// FIXME: Multiple buttons rendering
+// FIXME: Chat replay
+// FIXME: Multiple tabs breaks sometimes (check )
+// FIXME: Settings not vailable error in console
+// FIXME: Chat moves every refresh but should stay oin one side
+function injectButton() {
+	const chat = document.getElementById("channel-chatroom");
 
-				const chatSettingsList = document
-					.getElementById("chat-settings-panel")
-					.querySelector("ul");
-				let chatSettingsListItem;
+	if (chat) {
+		if (chat.parentElement !== currentChatroomParent) {
+			const chatSettingsList = document
+				.getElementById("chat-settings-panel")
+				.querySelector("ul");
+			let chatSettingsListItem;
 
-				const runtime =
-					typeof browser === "undefined" ? chrome.runtime : browser.runtime;
+			const runtime =
+				typeof browser === "undefined" ? chrome.runtime : browser.runtime;
 
-				if (chatSettingsList) {
-					chatSettingsListItem = chatSettingsList.firstChild.cloneNode(true);
+			if (chatSettingsList) {
+				chatSettingsListItem = chatSettingsList.firstChild.cloneNode(true);
 
-					chatSettingsList
-						.appendChild(chatSettingsListItem)
-						.querySelector("span").innerText = "Move The Chat";
+				chatSettingsList
+					.appendChild(chatSettingsListItem)
+					.querySelector("span").innerText = "Move The Chat";
 
-					let itemIcon = chatSettingsList.lastChild.querySelector("svg");
-					let itemSVG = document.createElement("img");
-					itemSVG.src = runtime.getURL("../icons/swap.svg");
-					itemSVG.width = 16;
-					itemSVG.height = 16;
+				let itemIcon = chatSettingsList.lastChild.querySelector("svg");
+				let itemSVG = document.createElement("img");
+				itemSVG.src = runtime.getURL("../icons/swap.svg");
+				itemSVG.width = 16;
+				itemSVG.height = 16;
 
-					chatSettingsListItem.firstChild.replaceChild(itemSVG, itemIcon);
-				}
-
-				let topBar = chat.firstChild;
-				let mtc = topBar.firstChild.cloneNode(true);
-				mtc.firstChild.firstChild.remove();
-
-				let svg = document.createElement("img");
-				svg.src = runtime.getURL("../icons/swap.svg");
-				svg.width = 20;
-				svg.height = 20;
-
-				mtc.firstChild.appendChild(svg);
-				topBar.appendChild(mtc);
-
-				currentChatroomParent = chat.parentElement;
-
-				if (chatSettingsList) {
-					chatSettingsListItem.addEventListener("click", moveChat);
-				}
-				mtc.addEventListener("click", moveChat);
-				moveChat();
-			}
-		}
-
-		// Stream change
-		const userCard = document.getElementById("user-identity");
-
-		if (userCard) {
-			const chatWidth =
-				getComputedStyle(userCard).getPropertyValue("--chat-width");
-			const sidebar = document.getElementById("sidebar-wrapper");
-			let sidebarWidth = 0;
-
-			if (sidebar) {
-				sidebarWidth = sidebar.offsetWidth;
+				chatSettingsListItem.firstChild.replaceChild(itemSVG, itemIcon);
 			}
 
-			storage.local.get("direction").then((res) => {
-				if (res.direction === "left") {
-					userCard.style.left = parseInt(chatWidth) + sidebarWidth + "px";
-				} else {
-					userCard.style.removeProperty("left");
-				}
-			});
-		}
+			let topBar = chat.firstChild;
+			let mtc = topBar.firstChild.cloneNode(true);
+			mtc.firstChild.firstChild.remove();
 
-		// HOTFIX: More than 2 elements?
-		// Theater mode
-		const video = document.getElementById("video-player");
-		const player = video.parentElement;
-		if (player) {
-			if (player.childElementCount !== currentPlayerChildren) {
-				currentPlayerChildren = player.childElementCount;
+			let svg = document.createElement("img");
+			svg.src = runtime.getURL("../icons/swap.svg");
+			svg.width = 20;
+			svg.height = 20;
 
-				if (currentPlayerChildren > 1) {
-					storage.local.get("direction").then((result) => {
-						const direction = result.direction || "right";
-						const controls = player.lastChild;
+			mtc.firstChild.appendChild(svg);
+			topBar.appendChild(mtc);
 
-						if (direction === "right") {
-							controls.classList.replace("left-0", "right-0");
-						} else {
-							controls.classList.replace("right-0", "left-0");
-						}
-					});
-				}
+			currentChatroomParent = chat.parentElement;
+
+			if (chatSettingsList) {
+				chatSettingsListItem.addEventListener("click", moveChat);
 			}
+			mtc.addEventListener("click", moveChat);
+			moveChat();
 		}
+	}
+}
+
+function init() {
+	const observer = new MutationObserver(() => {
+		injectButton();
+		moveOnStreamChange(); // useless?
+		moveUserCard();
+		moveVideoPlayer();
 	});
 
 	observer.observe(document.body, {
